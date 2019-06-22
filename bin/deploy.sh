99d1d40cb3aa3f169774e8d7f8a0aece11010579
odan <odan3240@gmail.com>
2019-06-23 07:11:47 +0900

    chore(bin): add deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+DIRECTORY=frontend/dist
+USER=odan
+EMAIL=odan3240@gmail.com
+REPOSITORY=git@github.com:odanado/ethereum-message-sandbox.git
+BRANCH=gh-pages
+
+DIST_DIR=$(dirname $0)/../${DIRECTORY}
+
+cd $DIST_DIR
+echo $(pwd)
+
+git init .
+git config --local user.name "odan"
+git config --local user.email "odan3240@gmail.com"
+
+git checkout -b ${BRANCH}
+git add ./
+git commit -m 'Update [ci skip]'
+git push -f ${REPOSITORY} ${BRANCH}
