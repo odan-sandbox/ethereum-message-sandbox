8cac63612bfb0ef35fd4c46bce39cd8b8cd0b460
odan <odan3240@gmail.com>
2019-08-05 01:38:12 +0900

    fix(plugins): ignore vuex-smart-module

--- frontend/plugins/vuex-smart-module.ts
+++ frontend/plugins/vuex-smart-module.ts
@@ -1,3 +1,4 @@
+/*
 import { Context } from '@nuxt/vue-app'
 import { registerModule } from 'vuex-smart-module'
 import { web3 } from '@/store/web3'
@@ -5,3 +6,4 @@ import { web3 } from '@/store/web3'
 export default ({ store }: Context) => {
   registerModule(store, 'poyo', 'poyo', web3.clone())
 }
+*/
